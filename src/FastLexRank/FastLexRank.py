565494c9a28ab249339640e3d7d631bcdb1b052b
maolee <maolee@umich.edu>
2023-11-01 21:13:59 -0400

    modify the cos_sim implementation

--- src/FastLexRank/FastLexRank.py
+++ src/FastLexRank/FastLexRank.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from sentence_transformers import SentenceTransformer
+from sentence_transformers.util import cos_sim
 import numpy as np
-
+import torch
 
 class FastLexRankSummarizer:
     """
@@ -14,21 +15,20 @@ class FastLexRankSummarizer:
 
     def __init__(
         self,
-        corpus: list[str],
         model_path: str = "all-MiniLM-L12-v2",
         threshold: float = None,
     ) -> None:
-        self.corpus = corpus
         self.model_path = model_path
         self.threshold = threshold
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model = SentenceTransformer(self.model_path, device=self.device)
 
-    def _get_sentence_embeddings(self) -> np.ndarray:
+    def _get_sentence_embeddings(self, corpus: list[str]) -> np.ndarray:
         """
         Calculate the sentence embeddings for the corpus
         :return: sentence embeddings
         """
-        model = SentenceTransformer(self.model_path)
-        embeddings = model.encode(self.corpus)
+        embeddings = self.model.encode(corpus)
         return embeddings
 
     def _get_similarity_matrix(self, embeddings: np.ndarray) -> np.ndarray:
@@ -37,17 +37,17 @@ class FastLexRankSummarizer:
         :param embeddings: sentence embeddings
         :return: cosine similarity matrix
         """
-        similarity_matrix = np.inner(embeddings, embeddings)
+        similarity_matrix = cos_sim(embeddings, embeddings)
         return similarity_matrix
 
-    def get_lexrank_scores(self) -> np.ndarray:
+    def get_lexrank_scores(self, corpus: list[str]) -> np.ndarray:
         """
         Calculate the LexRank score for each sentence
-        :param similarity_matrix: cosine similarity matrix
         :return: LexRank scores
         """
-        embeddings = self._get_sentence_embeddings()
+        embeddings = self._get_sentence_embeddings(corpus)
         similarity_matrix = self._get_similarity_matrix(embeddings)
+        similarity_matrix = np.array(similarity_matrix.cpu())
         if self.threshold:
             similarity_matrix[similarity_matrix < self.threshold] = 0
 
@@ -60,7 +60,7 @@ class FastLexRankSummarizer:
         approx_scores = np.dot(z.T, F)
         return approx_scores
 
-    def _get_top_sentences(self, lexrank_scores: np.ndarray, n: int = 3) -> list:
+    def _get_top_sentences(self, lexrank_scores: np.ndarray, n: int = 3) -> list[str]:
         """
         Return the top sentences with the highest LexRank score
         :param lexrank_scores: LexRank scores
@@ -70,16 +70,16 @@ class FastLexRankSummarizer:
         top_sentences = np.argsort(lexrank_scores)[::-1][:n]
         return top_sentences
 
-    def summarize(self, n: int = 3) -> list[str]:
+    def summarize(self, corpus: list[str], n: int = 3) -> list[str]:
         """
         Calculate the LexRank score for each sentence in the corpus and return the top sentences
         :param n: number of sentences to return
         :return: list of sentences with the highest LexRank score
         """
 
-        lexrank_scores = self.get_lexrank_scores()
+        lexrank_scores = self.get_lexrank_scores(corpus)
         top_sentences = self._get_top_sentences(lexrank_scores, n)
-        return [self.corpus[i] for i in top_sentences]
+        return [corpus[i] for i in top_sentences]
 
     def __call__(self, n: int = 3) -> list:
         """
@@ -119,5 +119,5 @@ if __name__ == "__main__":
         'incomes", and undertake "long-term structural reform" of the '
         "banking sector, education and the welfare state.",
     ]
-    summarizer = FastLexRankSummarizer(sentences)
-    print(summarizer.summarize())
+    summarizer = FastLexRankSummarizer()
+    print(summarizer.summarize(sentences))
